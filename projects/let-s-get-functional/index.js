b9666c23a87223e6212263aca774a752cf4b2235
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2025-07-17 17:42:34 -0500

    solved last three problems

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -98,11 +98,86 @@ var friendFirstLetterCount = function(array, customer, letter) {
 	return _.reduce(friendList, (count, elem) => elem.name[0].toLowerCase() === letter.toLowerCase() ? count + 1 : count, 0)
 };
 
-var friendsCount;
+var friendsCount = function(array, name) {
+	//illegible one-line solution
+	//return _.map(_.filter(array, (customer) => _.some(customer.friends, (friend) => friend.name === name)), (elem) => elem.name);
+	
+	//Step-by-step breakdown
+	
+	//the checkForFriend function uses _.some() to check if any of a given customer's friends are the target, and returns true if so, false if not
+	const checkForFriend = (customer) => _.some(customer.friends, (friend) => friend.name === name);
+	//pass checkForFriend as the callback to _.filter to filter the full array to just an array of customer objects who are friends with the target
+	const friendsOfTarget = _.filter(array, checkForFriend);
+	//use _.map to transform the friendsOfTarget array of customer objects to an array of customer names, and return
+	return _.map(friendsOfTarget, (elem) => elem.name);
+};
 
-var topThreeTags;
+var topThreeTags = function(array) {
+	//use _.reduce to count each tag
+	//reduce's memo is an object of form {tag: count}. The callback increments the count when it finds a repeated tag, and adds a tag with count one when it finds a new tag
+	//the initial value for the tagTracker object is an empty object because no tags have been seen yet
+	//the tagTracker must be returned by the callback function to be used on the next element
+	const tagCounts = _.reduce(array, (tagTracker, elem) => {
+		for (let tag of elem.tags) {
+			if (tagTracker.hasOwnProperty(tag)) { //increment previously seen tags
+				tagTracker[tag] += 1;
+			} else { //add new tags with count of one
+				tagTracker[tag] = 1;
+			}
+		}
+		return tagTracker;
+	}, {});
 
-var genderCount;
+	//initialize array to store top three current tags, along with their counts
+	let orderedTags = []; //nested array of form [ [tag, count], ... ]
+
+	//loop over every tag in the tagCount object
+	for (let tag in tagCounts) {
+		let count = tagCounts[tag]; //for convenience, bind the count to a variable
+
+		//loop over the array of leading tags/counts
+		for (let i = 0; i < orderedTags.length; i++) {
+			//if the current tag is more common than one of the leading tags, insert it ahead of that tag and end the loop
+			if (count >= orderedTags[i][1]) {
+				orderedTags.splice(i, 0, [tag, count]);
+				break;
+			}	
+		}
+
+		//special treatment for the first couple tags before orderedTags is built up
+		if (orderedTags.length === 0) {
+			//if orderedTags is empty, there's nothing to compare count to, so just stick it at the end
+			orderedTags.push([tag, count]);
+		} else if (orderedTags.length < 3 && count < orderedTags[orderedTags.length - 1][1]) {
+			//if orderedTags has less than three elements, then the current tag is one of the three leading tags by default, even it's tacked on in last place
+			orderedTags.push([tag, count]);
+		}
+		
+		//trim the array of leading tags to three
+		//we only need find the top three tags, not to sort them all, so this saves iterations over the orderedTags array
+		if (orderedTags.length > 3) {
+			orderedTags = orderedTags.slice(0, 3);
+		}
+	}
+
+	//use _.map to change the array of form [ [tag, count] ] to just an array of tags
+	return _.map(orderedTags, (elem) => elem[0]);
+};
+
+var genderCount = function(array) {
+	//this is pretty much the same as the tagTracker part of topThreeTags
+	//uses reduce to count the occurances of each gender
+	//if the memo object already has the customer's gender, the callback increments its count by one, otherwise it adds a new key with count one for the new gender
+	//the memo starts as an empty object
+	return _.reduce(array, (memo, elem) => {
+		if (memo.hasOwnProperty(elem.gender)) {
+			memo[elem.gender] += 1;
+		} else {
+			memo[elem.gender] = 1;
+		}
+		return memo;
+	}, {});	
+};
 
 //////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE ////////////////////////////////////////////
